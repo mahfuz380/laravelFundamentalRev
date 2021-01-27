0f33c46293138b811aaacbb4268563ae10d83fe7
mahfuz380 <almahfuz380@gmail.com>
2021-01-27 12:30:48 +0600

    mutators done

--- routes/web.php
+++ routes/web.php
@@ -364,6 +364,13 @@ Route::group(['middleware'=>'web'], function(){
 
         echo $user->name;
     });
+
+    Route::get('/setname', function(){
+        $user = User::find(1);
+
+        $user->name = "edwin";
+        $user->save();
+    });
     
 
 });
